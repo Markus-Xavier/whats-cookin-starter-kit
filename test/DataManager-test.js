053212f8aced15543e862db8f7a7fe86e46bcac3
Markus Rossio <argitavis@gmail.com>
2021-09-07 16:27:25 -0500

    Add tests to data manager for storing user db data

--- test/DataManager-test.js
+++ test/DataManager-test.js
@@ -14,22 +14,28 @@ import { filteredResultsByTagsAndKeywords } from './DataManagerTestDatabase';
 
 describe('DataManager', function() {
   let dataManager;
+  let user1;
+  let user2;
+  let userData;
 
   beforeEach(function() {
     dataManager = new DataManager();
+    user1 = {name: 'Dog Boy', id: 1337, pantry: [{ingredient: 12345, amount: 10}]};
+    user2 = {name: 'Cat Girl', id: 7331, pantry: [{ingredient: 54321, amount: 5}]};
+    userData = [user1, user2];
   });
 
   it('Should be a function', function() {
     assert.isFunction(DataManager);
   });
 
-  it('Should be an instance of DataManager', function(){
+  it('Should be an instance of DataManager', function() {
     assert.instanceOf(dataManager, DataManager);
   });
   
   it('should start with no recipes',  function() {
     assert.deepEqual(dataManager.recipes, []);
-  })
+  });
 
   it('should store recipes', function() {
     dataManager.setRecipes(recipes);
@@ -38,16 +44,16 @@ describe('DataManager', function() {
 
   it('should start off with no ingredients', function() {
     assert.deepEqual(dataManager.ingredients, []);
-  })
+  });
 
   it('should store ingredients', function() {
     dataManager.setIngredients(ingredients);
     assert.deepEqual(dataManager.ingredients, ingredients);
-  })
+  });
 
   it('should start off with no tags', function() {
     assert.deepEqual(dataManager.tags, []);
-  })
+  });
 
   it('should store tags from recipes', function() {
     dataManager.setRecipes(recipes);
@@ -63,39 +69,44 @@ describe('DataManager', function() {
       'condiment', 'dip',
       'spread'
     ]);
-  })
+  });
 
   it('should return a list of recipes filtered by tags and keywords', function () {
-   
-  dataManager.setRecipes(testRecipes);
-   dataManager.filterRecipes(userInput);
+    dataManager.setRecipes(testRecipes);
+    dataManager.filterRecipes(userInput);
 
     assert.deepEqual(dataManager.filteredRecipes, filteredResultsByTagsAndKeywords)
-  })
+  });
 
   it('should return a list of recipes filtered by keywords', function () {
     dataManager.setRecipes(testRecipes);
     dataManager.filterRecipes(userInput2);
 
     assert.deepEqual(dataManager.filteredRecipes, filteredResultsByKeywords)
-  })
+  });
 
   it('should return a list of recipes filtered by tags', function() {
     dataManager.setRecipes(testRecipes);
     dataManager.filterRecipes(userInput3);
 
-
     assert.deepEqual(dataManager.filteredRecipes, filteredResultsByTag)
-  })
+  });
 
 
   it('should check ingredients data and return a list of recipes with ingredients that match search keywords', function() {
     dataManager.setRecipes(testRecipes);
     dataManager.filterRecipes(userInput4);
 
-    assert.deepEqual(dataManager.filteredRecipes, filteredResultsByKeywords)
+    assert.deepEqual(dataManager.filteredRecipes, filteredResultsByKeywords);
+  });
 
-  })
+  it('should start with no users', function() {
+    assert.deepEqual(dataManager.users, []);
+  });
 
+  it('should be able to store users', function() {
+    dataManager.setUsers(userData);
 
+    assert.deepEqual(dataManager.users, userData);
+  });
 });
